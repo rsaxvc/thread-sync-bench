03468c1297579f1485c50634a4b4d697b5402e2c
Richard Allen <rsaxvc@gmail.com>
2013-07-18 22:48:35 -0500

    indent some code

--- bench.hpp
+++ bench.hpp
@@ -14,29 +14,29 @@ class benchmark
 		static void * threadrunner( void * vtcb );
 
 	protected:
-	struct thread_control_block
-		{
-		benchmark * bptr;
-		size_t num_points;
-		unsigned int rand_seed;
-		pthread_t id;
+		struct thread_control_block
+			{
+			benchmark * bptr;
+			size_t num_points;
+			unsigned int rand_seed;
+			pthread_t id;
 
-		/*Internal thread use*/
-		int32_t * in_circle;
-		int32_t * in_square;
-		};
+			/*Internal thread use*/
+			int32_t * in_circle;
+			int32_t * in_square;
+			};
 
-	//implement these for each syncronization primitive
-	virtual void prepare( size_t num_threads )=0;
-	virtual void prepare_thread( size_t thread_num, thread_control_block & tcb )=0;
-	virtual void count_circle(thread_control_block&)=0;
-	virtual void count_square(thread_control_block&)=0;
-	virtual void finish_thread(thread_control_block & tcb )=0;
-	virtual void finish(void)=0;
+		//implement these for each syncronization primitive
+		virtual void prepare( size_t num_threads )=0;
+		virtual void prepare_thread( size_t thread_num, thread_control_block & tcb )=0;
+		virtual void count_circle(thread_control_block&)=0;
+		virtual void count_square(thread_control_block&)=0;
+		virtual void finish_thread(thread_control_block & tcb )=0;
+		virtual void finish(void)=0;
 
-	//populate these in 'finish'
-	size_t total_pts_in_circle;
-	size_t total_pts_in_square;
+		//populate these in 'finish'
+		size_t total_pts_in_circle;
+		size_t total_pts_in_square;
 
 	public:
 		void run( size_t points, size_t threads );
