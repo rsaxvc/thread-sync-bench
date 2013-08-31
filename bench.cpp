4d151306d56c52e8dc5d8c3fbeddaa0163007dd0
Richard Allen <rsaxvc@gmail.com>
2013-08-31 17:12:11 -0500

    Clean up indentation

--- bench.cpp
+++ bench.cpp
@@ -18,18 +18,18 @@ benchmark * b = tcb.bptr;
 uint32_t r = xorshift_init;
 
 for( size_t i = 0; i < tcb.num_points; ++i )
-    {
+	{
 	r = xorshift( r );
-    uint16_t x = r & 0xFFF;
+	uint16_t x = r & 0xFFF;
 
 	r = xorshift( r );
-    uint16_t y = r & 0xFFF;
+	uint16_t y = r & 0xFFF;
 
-    if( pt_in_circle( x, y ) )
+	if( pt_in_circle( x, y ) )
 		b->count_circle( tcb );
 	else
 		b->count_square( tcb );
-    }
+	}
 
 return NULL;
 }
@@ -46,36 +46,36 @@ tcbs = new thread_control_block[num_threads];
 approx_points_per_thread = num_points / num_threads;
 num_points_remaining = num_points;
 for( size_t i = 0; i < num_threads; ++i )
-    {
-    tcbs[i].num_points = approx_points_per_thread;
-    num_points_remaining -= approx_points_per_thread;
+	{
+	tcbs[i].num_points = approx_points_per_thread;
+	num_points_remaining -= approx_points_per_thread;
 
-    tcbs[i].rand_seed = i;
+	tcbs[i].rand_seed = i;
 
 	tcbs[i].bptr = this;
 	prepare_thread( i, tcbs[i] );
     }
 while( num_points_remaining > 0 )
-    {
-    tcbs[ num_points % num_threads ].num_points ++;
-    num_points_remaining--;
-    }
+	{
+	tcbs[ num_points % num_threads ].num_points ++;
+	num_points_remaining--;
+	}
 
 for( size_t i = 0; i < num_threads; ++i )
-    {
-    int errnum = pthread_create( &tcbs[i].id, NULL, threadrunner, &tcbs[i] );
-    if( errnum )
-        {
-        errstr = "unable to create thread";
-        return -2;
-        }
-    }
+	{
+	int errnum = pthread_create( &tcbs[i].id, NULL, threadrunner, &tcbs[i] );
+	if( errnum )
+		{
+		errstr = "unable to create thread";
+		return -2;
+		}
+	}
 
 for( size_t i = 0; i < num_threads; ++i )
-    {
-    pthread_join( tcbs[i].id, NULL );
+	{
+	pthread_join( tcbs[i].id, NULL );
 	finish_thread( tcbs[i] );
-    }
+	}
 
 delete[]( tcbs );
 
