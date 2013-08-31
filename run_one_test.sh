06a3113e22b79d9dee9a96ec00d1226787857763
Richard Allen <rsaxvc@gmail.com>
2013-08-31 17:14:54 -0500

    extract context switches from time output

--- run_one_test.sh
+++ run_one_test.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
 
-`which time` -v ./bench $1 $2 $3
+tempfile=`mktemp`
+timepath=`which time`
+output=`$timepath -v --output=$tempfile ./bench $1 $2 $3`
 
+echo "Temp file located at:" . $tempfile
+
+vcs=`grep "Voluntary\ context\ switches" $tempfile`
+ics=`grep "Involuntary\ context\ switches" $tempfile`
+
+echo $vcs
+echo $ics
+
+###just some notes after here
+#print $output
+
+#thing="once upon a time"
+
+#set -- $thing
+
+#foo=$1
+#shift
+#bar=$@
+
+#echo "foo: $foo"
+#echo "bar: $bar"
+
+rm $tempfile
